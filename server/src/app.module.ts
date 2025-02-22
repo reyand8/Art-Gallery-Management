edb2b85a5a2e7e891adb79393b9980e4ce6ba655
Andrey <andreeey.gg@gmail.com>
2025-02-22 18:00:36 +0100

    feature-server: add env

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,20 +3,28 @@ import { SequelizeModule } from '@nestjs/sequelize';
 
 import { ArtworksModule } from './artworks/artworks.module';
 import { Artwork } from './models/artwork.model';
+import {ConfigModule, ConfigService} from "@nestjs/config";
 
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'art-galley',
-      models: [Artwork],
-      autoLoadModels: true,
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        dialect: configService.get<'postgres'>('DB_DIALECT'),
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        models: [Artwork],
+        autoLoadModels: true,
+        synchronize: true,
+      }),
     }),
     ArtworksModule
   ],
